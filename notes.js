fbd17d450efcd126105f48436ffe6ea3f242a4bd
Dealwap <nosisky@gmail.com>
2016-08-17 12:15:38 +0100

    Added The functions to create list

--- notes.js
+++ notes.js
@@ -8,15 +8,18 @@ class NotesApplication {
 		this.notes_content.push(notes);
 	}
 	listNotes(note_id, notes_content) {
-		return 
+		return notes;
 	}
 	 get(note_id) {
-		
+		note_id = [];
 	}
 	search(search_text) {
 	}
     delete(note_id) {
-		
+    var index = array.indexOf(notes);
+if (index > -1) {
+    array.splice(notes, 1);
+}
 	}
 	 edit(note_id, new_content) {
 		
