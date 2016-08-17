41851dbb1afb90ad4aaaa69b93b843a8436dadd1
Dealwap <nosisky@gmail.com>
2016-08-17 11:48:56 +0100

    Started Necessary Functions

--- notes.js
+++ notes.js
@@ -6,7 +6,20 @@ class NotesApplication {
 	function create(notes_content){
 		this.notes_content.push(notes);
 	}
-	function listNotes() {
+	function listNotes(note_id, notes_content) {
+		return 
+	}
+	function get(note_id) {
 		
+	}
+	function search(search_text) {
+		
+	function delete(note_id) {
+		
+	}
+	function edit(note_id, new_content) {
+		
+	}
+	
 	}
 }
