1aec094eb2786b1d8f8a226275f2d08a6c8548c7
Dealwap <nosisky@gmail.com>
2016-08-17 11:40:25 +0100

    Making Changes To Functions

---
+++ notes.js
@@ -0,0 +1,12 @@
+class NotesApplication {
+	constructor(author, notes)
+	this.author = author
+	this.notes = [];
+	
+	function create(notes_content){
+		this.notes_content.push(notes);
+	}
+	function listNotes() {
+		
+	}
+}
